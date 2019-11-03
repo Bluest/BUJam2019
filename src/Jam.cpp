7138dcc464e0fec9a5941cb3b497f910c8fd0236
Jack <jackpitman99@hotmail.co.uk>
2019-11-03 22:53:00 +0000

    Added the SDL_Mixer library and initialised it

--- src/Jam.cpp
+++ src/Jam.cpp
@@ -7,10 +7,33 @@ Jam::Jam()
 
 void Jam::init(SDL_Window* _window, SDL_Renderer* _renderer)
 {
-	SDL_Init(SDL_INIT_VIDEO);
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+	{
+		printf("SDL could not initialize! SDL Error:");
+		throw std::exception(SDL_GetError());
+	}
 
-	_window = SDL_CreateWindow("Jam", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+	//Initialize SDL_mixer
+	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
+	{
+		printf("SDL_Mixer could not initialize! SDL Error:");
+		throw std::exception(SDL_GetError());
+	}
+
+	_window = SDL_CreateWindow("Jam", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight,
+	                           SDL_WINDOW_SHOWN);
+	if (_window == nullptr)
+	{
+		printf("SDL Window could not be created SDL Error:");
+		throw std::exception(SDL_GetError());
+	}
+
 	_renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
+	if (_renderer == nullptr)
+	{
+		printf("SDL Renderer could not be created! SDL Error:");
+		throw std::exception(SDL_GetError());
+	}
 
 	SDL_RenderSetLogicalSize(_renderer, windowWidth / rendererScale, windowHeight / rendererScale);
 	SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
--- src/Jam.h
+++ src/Jam.h
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_mixer.h>
 #include <SDL_image.h>
 #include <iostream>
 #include <memory>
